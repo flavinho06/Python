93ba1eaf7a75b53c272d1857a6e605c087cdc3ea
Flávio Scanoni <juli.flavioscanoni@gmail.com>
2024-08-28 17:26:10 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,21 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Flávio") # comentário a linha do código
+print('Flávio')
+print('Flávio "Fafa" Scanoni')
+print("Flávio 'Fafa' Scanoni")
+print("Flávio \"Fafa\" Scanoni")
+
+teste = "oi" 
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
+
