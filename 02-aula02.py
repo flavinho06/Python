98e295a87a2d235186a44f6bc2059a99b2410a67
Flávio Scanoni <juli.flavioscanoni@gmail.com>
2024-09-11 17:09:34 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# Atribuição de variáveis
+nome = "Flavio"
+idade = "16"
+altura = "1.75"
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, "altura: ", idade ,)
+#Formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
